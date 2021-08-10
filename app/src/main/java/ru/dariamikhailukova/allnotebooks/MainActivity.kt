98c8e87fa05f04a8f01da28ac3afa0cf13291537
Daria <dm38@inbox.ru>
2021-08-10 14:39:58 +0300

    Обработка исключений при выборе task

--- app/src/main/java/ru/dariamikhailukova/allnotebooks/MainActivity.kt
+++ app/src/main/java/ru/dariamikhailukova/allnotebooks/MainActivity.kt
@@ -1,5 +1,6 @@
 package ru.dariamikhailukova.allnotebooks
 
+import android.content.ActivityNotFoundException
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ru.dariamikhailukova.allnotebooks.databinding.ActivityMainBinding
@@ -18,53 +19,91 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent()
 
         binding.buttonTask1.setOnClickListener {
-            intent.setClassName("ru.dariamikhailukova.task1",
-                "ru.dariamikhailukova.task1.view.MainActivityView")
-            startActivity(intent)
+
+            try {
+                intent.setClassName("ru.dariamikhailukova.task1",
+                    "ru.dariamikhailukova.task1.view.MainActivityView")
+                startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+                e.printStackTrace()
+            }
         }
 
         binding.buttonTask2.setOnClickListener {
-            intent.setClassName("ru.dariamikhailukova.task2",
-                "ru.dariamikhailukova.task2.view.MainActivityView")
-            startActivity(intent)
+
+            try {
+                intent.setClassName("ru.dariamikhailukova.task2",
+                    "ru.dariamikhailukova.task2.view.MainActivityView")
+                startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+                e.printStackTrace()
+            }
         }
 
         binding.buttonTask3.setOnClickListener {
-            intent.setClassName("ru.dariamikhailukova.task3",
-                "ru.dariamikhailukova.task3.MainActivity")
-            startActivity(intent)
+
+            try {
+                intent.setClassName("ru.dariamikhailukova.task3",
+                    "ru.dariamikhailukova.task3.MainActivity")
+                startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+                e.printStackTrace()
+            }
         }
 
         binding.buttonTask4.setOnClickListener {
-            intent.setClassName("ru.dariamikhailukova.task4",
-                "ru.dariamikhailukova.task4.MainActivity")
-            startActivity(intent)
+
+            try {
+                intent.setClassName("ru.dariamikhailukova.task4",
+                    "ru.dariamikhailukova.task4.MainActivity")
+                startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+                e.printStackTrace()
+            }
         }
 
         binding.buttonTask5.setOnClickListener {
-            intent.setClassName("ru.dariamikhailukova.task5",
-                "ru.dariamikhailukova.task5.MainActivity")
-            startActivity(intent)
+
+            try {
+                intent.setClassName("ru.dariamikhailukova.task5",
+                    "ru.dariamikhailukova.task5.MainActivity")
+                startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+                e.printStackTrace()
+            }
         }
 
         binding.buttonTask6.setOnClickListener {
-            intent.setClassName("ru.dariamikhailukova.task6",
-                "ru.dariamikhailukova.task6.MainActivity")
-            startActivity(intent)
+
+            try {
+                intent.setClassName("ru.dariamikhailukova.task6",
+                    "ru.dariamikhailukova.task6.MainActivity")
+                startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+                e.printStackTrace()
+            }
         }
 
         binding.buttonTask7.setOnClickListener {
-            intent.setClassName("ru.dariamikhailukova.task7",
-                "ru.dariamikhailukova.task7.MainActivity")
-            startActivity(intent)
+
+            try {
+                intent.setClassName("ru.dariamikhailukova.task7",
+                    "ru.dariamikhailukova.task7.MainActivity")
+                startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+                e.printStackTrace()
+            }
         }
 
         binding.buttonTask8.setOnClickListener {
-            intent.setClassName("ru.dariamikhailukova.task8",
-                "ru.dariamikhailukova.task8.MainActivity")
-            startActivity(intent)
+
+            try {
+                intent.setClassName("ru.dariamikhailukova.task8",
+                    "ru.dariamikhailukova.task8.MainActivity")
+                startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+                e.printStackTrace()
+            }
         }
-
-
     }
 }
